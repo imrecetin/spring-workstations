71499875bbf7d9079c767e36b98c94b504ecea8f
Cetin Imre <ctn.imre@gmail.com>
2019-06-07 21:12:09 +0300

    init

--- spring-boot-redis/src/main/java/com/spring/redis/RedisApplication.java
+++ spring-boot-redis/src/main/java/com/spring/redis/RedisApplication.java
@@ -1,4 +1,4 @@
-package com.spring.redis.redis;
+package com.spring.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
