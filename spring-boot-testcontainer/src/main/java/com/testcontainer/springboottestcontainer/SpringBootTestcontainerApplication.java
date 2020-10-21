78044ae4f081a99f384a9cc32bad8f52de333a99
Çetin İmre <ctn.imre@gmail.com>
2020-10-21 23:16:18 +0300

    Init Test Container

---
+++ spring-boot-testcontainer/src/main/java/com/testcontainer/springboottestcontainer/SpringBootTestcontainerApplication.java
@@ -0,0 +1,13 @@
+package com.testcontainer.springboottestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestcontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestcontainerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-testcontainer/src/test/java/com/testcontainer/springboottestcontainer/SpringBootTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcontainer.springboottestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
