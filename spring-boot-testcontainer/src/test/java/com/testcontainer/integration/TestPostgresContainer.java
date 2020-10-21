20d5e488847b252fa20cb2717b8c58a53a96a6f7
Çetin İmre <ctn.imre@gmail.com>
2020-10-22 00:13:29 +0300

    Add TestPostgresContainer

--- spring-boot-testcontainer/src/test/java/com/testcontainer/integration/TestPostgresContainer.java
+++ spring-boot-testcontainer/src/test/java/com/testcontainer/integration/TestPostgresContainer.java
@@ -1,4 +1,36 @@
 package com.testcontainer.integration;
 
-public class TestPostgresContainer {
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class TestPostgresContainer extends PostgreSQLContainer<TestPostgresContainer> {
+
+    private static final String IMAGE_VERSION = "postgres:9.5";
+    private static TestPostgresContainer container;
+
+    private TestPostgresContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    public static TestPostgresContainer getInstance() {
+        if (container == null) {
+            container = new TestPostgresContainer();
+        }
+        container.withDatabaseName("integration-tests-db");
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+        System.setProperty("DB_NAME", container.getDatabaseName());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+
 }
