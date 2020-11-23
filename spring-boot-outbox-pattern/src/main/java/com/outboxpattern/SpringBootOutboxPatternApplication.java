6943b2d0beeeb22f30914b3b7adb9b960ec37381
Çetin İmre <ctn.imre@gmail.com>
2020-11-24 00:18:08 +0300

    init Spring Boot project

---
+++ spring-boot-outbox-pattern/src/main/java/com/outboxpattern/SpringBootOutboxPatternApplication.java
@@ -0,0 +1,13 @@
+package com.outboxpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOutboxPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOutboxPatternApplication.class, args);
+	}
+
+}
---
+++ spring-boot-outbox-pattern/src/test/java/com/outboxpattern/SpringBootOutboxPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outboxpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOutboxPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
