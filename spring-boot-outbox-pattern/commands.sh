48bc4d09428abd6cbc82d78275b5454cb955211e
Çetin İmre <ctn.imre@gmail.com>
2020-11-25 01:06:30 +0300

    Add some beneficial commands for docker and postgress

--- spring-boot-outbox-pattern/commands.sh
+++ spring-boot-outbox-pattern/commands.sh
@@ -1,1 +1,17 @@
 docker-compose up -d
+
+docker exec -it postgres_order psql -U order_user -W orderdb
+docker exec -it postgres_shipment psql -U shipment_user -W shipmentdb
+
+\? list all the commands
+\l list databases
+\conninfo display information about current connection
+\c [DBNAME] connect to new database, e.g., \c template1
+\dt list tables of the public schema
+\dt <schema-name>.* list tables of certain schema, e.g., \dt public.*
+\dt *.* list tables of all schemas
+Then you can run SQL statements, e.g., SELECT * FROM my_table;(Note: a statement must be terminated with semicolon ;)
+\q quit psql
+
+select * from public.databasechangelog;
+select * from public.order;
